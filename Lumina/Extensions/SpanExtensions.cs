7b2815498ddbb51c6a0a1418b2f4818d84b0f332
NotAdam <adam@notadam.me>
2020-02-16 20:31:49 +1100

    bit of cleanup, use conditional itemgroup for .net standard packages

--- Lumina/Extensions/SpanExtensions.cs
+++ Lumina/Extensions/SpanExtensions.cs
@@ -6,32 +6,30 @@ namespace Lumina.Extensions
 {
     public static class SpanExtensions
     {
-#if NETSTANDARD
+
         public static unsafe T ReadStructure< T >( this Span< byte > span ) where T : struct
         {
+#if NETSTANDARD
             fixed( byte* bp = &span.GetPinnableReference() )
             {
                 return Marshal.PtrToStructure< T >( new IntPtr( bp ) );
             }
+#else
+            return MemoryMarshal.Read< T >( span );
+#endif
         }
 
         public static unsafe T ReadStructure< T >( this Span< byte > span, int offset ) where T : struct
         {
+#if NETSTANDARD
             fixed( byte* bp = &span.GetPinnableReference() )
             {
                 return Marshal.PtrToStructure< T >( new IntPtr( bp + offset ) );
             }
-        }
-#elif NETCOREAPP
-        public static T ReadStructure< T >( this Span< byte > span ) where T : struct
-        {
-            return MemoryMarshal.AsRef< T >( span );
-        }
-
-        public static T ReadStructure< T >( this Span< byte > span, int offset ) where T : struct
-        {
-            return MemoryMarshal.AsRef< T >( span.Slice( offset ) );
-        }
+#else
+            return MemoryMarshal.Read< T >( span );
 #endif
+
+        }
     }
 }
