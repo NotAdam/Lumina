a7d71fe45f8e0ba69f4a435234ed593a412eec87
Adam <893184+NotAdam@users.noreply.github.com>
2020-12-11 20:54:10 +1100

    basic shit sestring impl

---
+++ src/Lumina/Text/Payloads/BasePayload.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lumina.Text.Payloads
+{
+    public class BasePayload
+    {
+        public BasePayload()
+        {
+            
+        }
+        
+        public BasePayload( byte[] data )
+        {
+            _data = data;
+            _rawString = Encoding.UTF8.GetString( data );
+        }
+
+        public BasePayload( BinaryReader br )
+        {
+            var data = GetPayloadData( br );
+
+            if( data.Any() )
+            {
+                _rawString = Encoding.UTF8.GetString( data );
+            }
+        }
+
+        public byte[] GetPayloadData( BinaryReader br )
+        {
+            var data = new List< byte >();
+            
+            while( br.BaseStream.Position < br.BaseStream.Length )
+            {
+                var d = br.ReadByte();
+                data.Add( d );
+                if( d == SeString.END_BYTE )
+                {
+                    // data.Add( d );
+                    // br.BaseStream.Position++;
+                    break;
+                }
+            }
+
+            return data.ToArray();
+        }
+        
+        #if DEBUG
+        public string PayloadType => GetType().FullName;
+        #endif
+        
+        protected byte[] _data;
+        protected string _rawString;
+
+        public ReadOnlySpan< byte > Data => _data.AsSpan();
+        public string RawString => _rawString;
+
+    }
+}
