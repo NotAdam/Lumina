0a98746e23687b98a8d8ce3cfa0c799ae77d1964
Liam <lmcintyre995@gmail.com>
2021-04-04 02:37:00 -0400

    Terrain files

---
+++ src/Lumina/Data/Files/TeraFile.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+using Lumina.Extensions;
+
+namespace Lumina.Data.Files
+{
+    struct PlatePos
+    {
+        public short x;
+        public short y;
+    }
+    
+    public class TeraFile : FileResource
+    {
+        public uint Version;
+        public uint PlateCount;
+        public uint PlateSize;
+        public float ClipDistance;
+        public float Unknown;
+        public byte[] Padding;
+
+        private PlatePos[] _positions; 
+        
+        public override void LoadFile()
+        {
+            Version = Reader.ReadUInt32();
+            PlateCount = Reader.ReadUInt32();
+            PlateSize = Reader.ReadUInt32();
+            ClipDistance = Reader.ReadSingle();
+            Unknown = Reader.ReadSingle();
+            Padding = Reader.ReadBytes( 32 );
+
+            _positions = Reader.ReadStructuresAsArray< PlatePos >( (int) PlateCount );
+        }
+
+        /// <summary>
+        /// Retrieve the X and Z coordinates of the specified plate index. Note that
+        /// the Y coordinate is unnecessary as bg plates each contain all necessary vertical
+        /// data in their respective plate.
+        /// </summary>
+        /// <param name="plateIndex">The index of the bg plate to obtain the coordinates for.</param>
+        /// <returns></returns>
+        public Vector2 GetPlatePosition( int plateIndex )
+        {
+            var pos = _positions[ plateIndex ];
+            return new Vector2( PlateSize * ( pos.x + 0.5f ), PlateSize * ( pos.y + 0.5f ) );
+        }
+    }
+}
