1cd10faaec757210e4d3957a7011bc7e6c370dd2
pmgr <26606291+pmgr@users.noreply.github.com>
2021-05-05 21:42:39 +0100

    Add rudimentary support for PCB files

---
+++ src/Lumina/Data/Files/Pcb/PcbListFile.cs
@@ -0,0 +1,67 @@
+using System.IO;
+using Lumina.Data.Attributes;
+using Lumina.Data.Parsing;
+
+namespace Lumina.Data.Files.Pcb
+{
+    [FileExtension( "list.pcb" )]
+    public class PcbListFile : FileResource
+    {
+        public struct PcbNodeList {
+            public Common.BoundingBox BoundingBox;
+            public ListNode[] Children;
+            
+            public static PcbNodeList Read( BinaryReader reader )
+            {
+                var nodeList = new PcbNodeList
+                {
+                    Children = new ListNode[reader.ReadUInt32()],
+                    BoundingBox = Common.BoundingBox.Read( reader )
+                };
+
+                // Padding
+                reader.BaseStream.Position += 4;
+                
+                for( var i = 0; i < nodeList.Children.Length; i++ )
+                {
+                    nodeList.Children[ i ] = ListNode.Read( reader );
+                }
+
+                return nodeList;
+            }
+        }
+        
+        public struct ListNode
+        {
+            public uint Id;
+            public Common.BoundingBox BoundingBox;
+
+            public static ListNode Read( BinaryReader reader )
+            {
+                var node = new ListNode
+                {
+                    Id = reader.ReadUInt32(),
+                    BoundingBox = Common.BoundingBox.Read( reader )
+                };
+
+                // Padding
+                reader.BaseStream.Position += 4;
+
+                return node;
+            }
+        }
+
+        public PcbNodeList Nodes;
+
+        public override void LoadFile()
+        {
+            var streamStart = Reader.BaseStream.Position;
+            var isList = Reader.ReadUInt32() != 0;
+            Reader.BaseStream.Position = streamStart;
+
+            if( !isList ) return;
+            
+            Nodes = PcbNodeList.Read( Reader );
+        }
+    }
+}
