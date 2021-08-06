ce385133a571de23df702a3c42a103b0ab7d28ab
Adam <893184+NotAdam@users.noreply.github.com>
2021-08-06 22:33:51 +1000

    use uint for icon ids

--- src/Lumina/Extensions/LuminaExtensions.cs
+++ src/Lumina/Extensions/LuminaExtensions.cs
@@ -4,14 +4,14 @@ namespace Lumina.Extensions
 {
     public static class LuminaExtensions
     {
-        public static TexFile? GetIcon( this GameData gameData, int iconId )
+        public static TexFile? GetIcon( this GameData gameData, uint iconId )
         {
             var path = $"ui/icon/{iconId / 1000 * 1000:000000}/{iconId:000000}.tex";
 
             return gameData.GetFile< TexFile >( path );
         }
         
-        public static TexFile? GetHqIcon( this GameData gameData, int iconId )
+        public static TexFile? GetHqIcon( this GameData gameData, uint iconId )
         {
             var path = $"ui/icon/{iconId / 1000 * 1000:000000}/{iconId:000000}_hr1.tex";
 
