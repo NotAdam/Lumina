baa0e2f1cb9b356ed72883935bb10616c5321728
Soreepeong <Soreepeong@gmail.com>
2024-04-10 12:57:03 +0900

    Doc fix

--- src/Lumina/Text/SeStringBuilder.UIntColors.cs
+++ src/Lumina/Text/SeStringBuilder.UIntColors.cs
@@ -25,6 +25,14 @@ public sealed partial class SeStringBuilder
     /// if the naming gets confusing.</remarks>
     public SeStringBuilder AppendBgraIntExpressionFromRgba( byte r, byte g, byte b, byte a ) => AppendBgraIntExpression( b, g, r, a );
 
+    /// <summary>Appends a BGRA integer calculated from RGBA values as an expression.</summary>
+    /// <param name="rgba">The RGBA color value, which is 0xAABBGGRR (0xRR 0xGG 0xBB 0xAA) on Windows version.</param>
+    /// <returns>A reference of this instance after the append operation is completed.</returns>
+    /// <remarks>See <a href="https://learn.microsoft.com/en-us/windows/win32/wic/-wic-codec-native-pixel-formats#rgbbgr-color-model">RGB/BGR color model</a>
+    /// if the naming gets confusing.</remarks>
+    public SeStringBuilder AppendBgraIntExpressionFromRgba( uint rgba ) =>
+        AppendUIntExpression( ( rgba & 0xFF00FF00 ) | ( rgba >> 16 ) | ( ( rgba & 0xFF ) << 16 ) );
+
     /// <summary>Appends a BGRA integer calculated from BGRA values as an expression.</summary>
     /// <param name="rgba">A normalized RGBA <see cref="System.Numerics.Vector4"/> value to append.</param>
     /// <returns>A reference of this instance after the append operation is completed.</returns>
@@ -92,15 +100,15 @@ public sealed partial class SeStringBuilder
     /// <remarks>See <a href="https://learn.microsoft.com/en-us/windows/win32/wic/-wic-codec-native-pixel-formats#rgbbgr-color-model">RGB/BGR color model</a>
     /// if the naming gets confusing.</remarks>
     public SeStringBuilder PushColorRgba( uint rgba ) =>
-        BeginMacro( MacroCode.Color ).AppendUIntExpression( ( rgba & 0xFF00FF00 ) | ( rgba >> 16 ) | ( ( rgba & 0xFF ) << 16 ) ).EndMacro();
+        BeginMacro( MacroCode.Color ).AppendBgraIntExpressionFromRgba( rgba ).EndMacro();
 
     /// <summary>Pushes a text foreground color.</summary>
-    /// <param name="bgra">The BGRA color value, which is 0xAARRGGBB (0xBB 0xGG 0xRR 0xAA) on Windows version.</param>
+    /// <param name="rgba">The RGBA color value, which is 0xAABBGGRR (0xRR 0xGG 0xBB 0xAA) on Windows version.</param>
     /// <returns>A reference of this instance after the push operation is completed.</returns>
     /// <remarks>See <a href="https://learn.microsoft.com/en-us/windows/win32/wic/-wic-codec-native-pixel-formats#rgbbgr-color-model">RGB/BGR color model</a>
     /// if the naming gets confusing.</remarks>
-    public SeStringBuilder PushColorRgba( Vector4 bgra ) =>
-        BeginMacro( MacroCode.Color ).AppendBgraIntExpressionFromRgba( bgra ).EndMacro();
+    public SeStringBuilder PushColorRgba( Vector4 rgba ) =>
+        BeginMacro( MacroCode.Color ).AppendBgraIntExpressionFromRgba( rgba ).EndMacro();
 
     /// <summary>Pops a text foreground color.</summary>
     /// <returns>A reference of this instance after the pop operation is completed.</returns>
@@ -151,15 +159,15 @@ public sealed partial class SeStringBuilder
     /// <remarks>See <a href="https://learn.microsoft.com/en-us/windows/win32/wic/-wic-codec-native-pixel-formats#rgbbgr-color-model">RGB/BGR color model</a>
     /// if the naming gets confusing.</remarks>
     public SeStringBuilder PushEdgeColorRgba( uint rgba ) =>
-        BeginMacro( MacroCode.EdgeColor ).AppendUIntExpression( ( rgba & 0xFF00FF00 ) | ( rgba >> 16 ) | ( ( rgba & 0xFF ) << 16 ) ).EndMacro();
+        BeginMacro( MacroCode.EdgeColor ).AppendBgraIntExpressionFromRgba( rgba ).EndMacro();
 
     /// <summary>Pushes a text edge color.</summary>
-    /// <param name="bgra">The BGRA color value, which is 0xAARRGGBB (0xBB 0xGG 0xRR 0xAA) on Windows version.</param>
+    /// <param name="rgba">The RGBA color value, which is 0xAABBGGRR (0xRR 0xGG 0xBB 0xAA) on Windows version.</param>
     /// <returns>A reference of this instance after the push operation is completed.</returns>
     /// <remarks>See <a href="https://learn.microsoft.com/en-us/windows/win32/wic/-wic-codec-native-pixel-formats#rgbbgr-color-model">RGB/BGR color model</a>
     /// if the naming gets confusing.</remarks>
-    public SeStringBuilder PushEdgeColorRgba( Vector4 bgra ) =>
-        BeginMacro( MacroCode.EdgeColor ).AppendBgraIntExpressionFromRgba( bgra ).EndMacro();
+    public SeStringBuilder PushEdgeColorRgba( Vector4 rgba ) =>
+        BeginMacro( MacroCode.EdgeColor ).AppendBgraIntExpressionFromRgba( rgba ).EndMacro();
 
     /// <summary>Pops a text border color.</summary>
     /// <returns>A reference of this instance after the pop operation is completed.</returns>
@@ -210,15 +218,15 @@ public sealed partial class SeStringBuilder
     /// <remarks>See <a href="https://learn.microsoft.com/en-us/windows/win32/wic/-wic-codec-native-pixel-formats#rgbbgr-color-model">RGB/BGR color model</a>
     /// if the naming gets confusing.</remarks>
     public SeStringBuilder PushShadowColorRgba( uint rgba ) =>
-        BeginMacro( MacroCode.ShadowColor ).AppendUIntExpression( ( rgba & 0xFF00FF00 ) | ( rgba >> 16 ) | ( ( rgba & 0xFF ) << 16 ) ).EndMacro();
+        BeginMacro( MacroCode.ShadowColor ).AppendBgraIntExpressionFromRgba( rgba ).EndMacro();
 
     /// <summary>Pushes a text shadow color.</summary>
-    /// <param name="bgra">The BGRA color value, which is 0xAARRGGBB (0xBB 0xGG 0xRR 0xAA) on Windows version.</param>
+    /// <param name="rgba">The RGBA color value, which is 0xAABBGGRR (0xRR 0xGG 0xBB 0xAA) on Windows version.</param>
     /// <returns>A reference of this instance after the push operation is completed.</returns>
     /// <remarks>See <a href="https://learn.microsoft.com/en-us/windows/win32/wic/-wic-codec-native-pixel-formats#rgbbgr-color-model">RGB/BGR color model</a>
     /// if the naming gets confusing.</remarks>
-    public SeStringBuilder PushShadowColorRgba( Vector4 bgra ) =>
-        BeginMacro( MacroCode.ShadowColor ).AppendBgraIntExpressionFromRgba( bgra ).EndMacro();
+    public SeStringBuilder PushShadowColorRgba( Vector4 rgba ) =>
+        BeginMacro( MacroCode.ShadowColor ).AppendBgraIntExpressionFromRgba( rgba ).EndMacro();
 
     /// <summary>Pops a text shadow color.</summary>
     /// <returns>A reference of this instance after the pop operation is completed.</returns>
