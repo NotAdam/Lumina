271cfde921694620dfade22e1bbab43746e3750f
Asriel Camora <asriel@camora.dev>
2024-08-14 14:15:14 -0700

    Add ExcelPage documentation

--- src/Lumina/Excel/ExcelPage.cs
+++ src/Lumina/Excel/ExcelPage.cs
@@ -16,6 +16,9 @@ namespace Lumina.Excel;
 [EditorBrowsable( EditorBrowsableState.Advanced )]
 public sealed class ExcelPage
 {
+    /// <summary>
+    /// The module that this page belongs to.
+    /// </summary>
     public ExcelModule Module { get; }
 
     private readonly byte[] data;
@@ -38,6 +41,12 @@ public sealed class ExcelPage
     private static float ReverseEndianness( float v ) =>
         Unsafe.BitCast<uint, float>( BinaryPrimitives.ReverseEndianness( Unsafe.BitCast<float, uint>( v ) ) );
 
+    /// <summary>
+    /// Reads a <see cref="ReadOnlySeString"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <param name="structOffset">Offset of the row inside the page.</param>
+    /// <returns>The <see cref="ReadOnlySeString"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public ReadOnlySeString ReadString( nuint offset, nuint structOffset )
     {
@@ -47,46 +56,102 @@ public sealed class ExcelPage
         return new ReadOnlySeString( data[..stringLength] );
     }
 
+    /// <summary>
+    /// Reads a <see cref="bool"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="bool"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public bool ReadBool( nuint offset ) =>
         Read<bool>( offset );
 
+    /// <summary>
+    /// Reads a <see cref="sbyte"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="sbyte"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public sbyte ReadInt8( nuint offset ) =>
         Read<sbyte>( offset );
 
+    /// <summary>
+    /// Reads a <see cref="byte"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="byte"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public byte ReadUInt8( nuint offset ) =>
         Read<byte>( offset );
 
+    /// <summary>
+    /// Reads a <see cref="short"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="short"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public short ReadInt16( nuint offset ) =>
         BinaryPrimitives.ReverseEndianness( Read<short>( offset ) );
 
+    /// <summary>
+    /// Reads a <see cref="ushort"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="ushort"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public ushort ReadUInt16( nuint offset ) =>
         BinaryPrimitives.ReverseEndianness( Read<ushort>( offset ) );
 
+    /// <summary>
+    /// Reads a <see cref="int"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="int"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public int ReadInt32( nuint offset ) =>
         BinaryPrimitives.ReverseEndianness( Read<int>( offset ) );
 
+    /// <summary>
+    /// Reads a <see cref="uint"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="uint"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public uint ReadUInt32( nuint offset ) =>
         BinaryPrimitives.ReverseEndianness( Read<uint>( offset ) );
 
+    /// <summary>
+    /// Reads a <see cref="float"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="float"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public float ReadFloat32( nuint offset ) =>
         ReverseEndianness( Read<float>( offset ) );
 
+    /// <summary>
+    /// Reads a <see cref="long"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="long"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public long ReadInt64( nuint offset ) =>
         BinaryPrimitives.ReverseEndianness( Read<long>( offset ) );
 
+    /// <summary>
+    /// Reads a <see cref="ulong"/> from the page data at <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the field inside the page.</param>
+    /// <returns>The <see cref="ulong"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public ulong ReadUInt64( nuint offset ) =>
         BinaryPrimitives.ReverseEndianness( Read<ulong>( offset ) );
 
+    /// <summary>
+    /// Reads a <see cref="bool"/> from the page data at <paramref name="offset"/> at bit offset <paramref name="bit"/>.
+    /// </summary>
+    /// <param name="offset">Byte offset of the field inside the page.</param>
+    /// <param name="bit">Bit offset of the field inside the byte. (0 - 7)</param>
+    /// <returns>The <see cref="ulong"/>.</returns>
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public bool ReadPackedBool( nuint offset, byte bit ) =>
         ( Read<byte>( offset ) & ( 1 << bit ) ) != 0;
