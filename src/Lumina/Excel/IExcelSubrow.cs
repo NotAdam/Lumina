287bc57cdebe498915e1852ba2811283fc4bd3ea
Asriel Camora <asriel@camora.dev>
2024-08-17 13:38:30 -0700

    Split IExcelRow.cs

--- src/Lumina/Excel/IExcelSubrow.cs
+++ src/Lumina/Excel/IExcelSubrow.cs
@@ -1,13 +1,16 @@
 namespace Lumina.Excel;
 
 /// <summary>
-/// Defines a row type/schema for an excel sheet.
+/// Defines a subrow type/schema for an excel sheet.
 /// </summary>
 /// <typeparam name="T">The type that implements the interface.</typeparam>
-public interface IExcelRow< out T > where T : struct
+public interface IExcelSubrow< out T > where T : struct
 {
     /// <summary>Gets the row ID.</summary>
     uint RowId { get; }
+
+    /// <summary>Gets the subrow ID.</summary>
+    ushort SubrowId { get; }
 
     /// <summary>
     /// Creates an instance of the current type. Designed only for use within <see cref="Lumina"/>.
@@ -15,6 +18,7 @@ public interface IExcelRow< out T > where T : struct
     /// <param name="page"></param>
     /// <param name="offset"></param>
     /// <param name="row"></param>
-    /// <returns>A newly created row object.</returns>
-    abstract static T Create( ExcelPage page, uint offset, uint row );
+    /// <param name="subrow"></param>
+    /// <returns>A newly created subrow object.</returns>
+    abstract static T Create( ExcelPage page, uint offset, uint row, ushort subrow );
 }
